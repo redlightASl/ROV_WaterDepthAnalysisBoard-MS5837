a495f35abac3bd7ab027040bcc2113081a628d03
Redlight <dddbbbdd@foxmail.com>
2022-07-03 18:23:25 +0800

    changed frame data

--- WaterDepthv2STP/Main/main.c
+++ WaterDepthv2STP/Main/main.c
@@ -9,7 +9,7 @@
 u8 ENABLE_SEND = 0;
 
 u16 u_temp = 0;
-u16 u_pres = 0;
+u32 u_pres = 0;
 u16 u_depth = 0;
     
 void TIM3_Int_Init(u16 arr,u16 psc);
@@ -37,26 +37,68 @@ int main(void)
     
     while(1)
     {
-        
         MS5837_getData(&temp, &pres);
         LED=0;
         MS5837_ReadDepth_filtered(&depth);
-        u_temp = (u16)temp;
-        u_pres = (u16)pres;
-        u_depth = depth*1000;
         
-        if(KEY_IN == 0) //debug mode
+        u_temp = (u16)(temp * 100);
+        u_pres = (u32)(pres * 100);
+        if(depth < 0)
         {
-            printf("%d,%d,%d\r\n", u_temp, u_pres, u_depth);
+            u_depth = (u16)(-depth * 1000);
         }
-        else if(ENABLE_SEND == 1)
+        else
         {
-            UART_Trans((u8)((u_temp&0xFF00)>>8));
-            UART_Trans((u8)u_temp&0x00FF);
-            UART_Trans((u8)((u_pres&0xFF00)>>8));
-            UART_Trans((u8)u_pres&0x00FF);
-            UART_Trans((u8)((u_depth&0xFF00)>>8));
-            UART_Trans((u8)u_depth&0x00FF);
+            u_depth = (u16)(depth * 1000);
+        }
+        
+        if(ENABLE_SEND == 1)
+        {
+            if(KEY_IN == 0) //debug mode
+            {
+//                printf("%f,%f,%f\r\n", temp, pres, depth);
+                if(depth < 0)
+                {
+                    printf("%d,%d,-%d\r\n", u_temp, u_pres, u_depth);
+                }
+                else
+                {
+                    printf("%d,%d,%d\r\n", u_temp, u_pres, u_depth);
+                }
+            }
+            else
+            {
+                if(depth < 0)
+                {
+                    UART_Trans(0x21);
+                    UART_Trans((u8)((u_temp&0xFF00)>>8));
+                    UART_Trans((u8)u_temp&0x00FF);
+                    UART_Trans((u8)((u_pres&0xFF000000)>>24));
+                    UART_Trans((u8)((u_pres&0x00FF0000)>>16));
+                    UART_Trans((u8)((u_pres&0x0000FF00)>>8));
+                    UART_Trans((u8)u_pres&0x000000FF);
+                    UART_Trans(0x00);
+                    UART_Trans((u8)((u_depth&0xFF00)>>8));
+                    UART_Trans((u8)u_depth&0x00FF);
+                    UART_Trans(0xFF);
+                    UART_Trans(0xFF);
+                }
+                else
+                {
+                    UART_Trans(0x21);
+                    UART_Trans((u8)((u_temp&0xFF00)>>8));
+                    UART_Trans((u8)u_temp&0x00FF);
+                    UART_Trans((u8)((u_pres&0xFF000000)>>24));
+                    UART_Trans((u8)((u_pres&0x00FF0000)>>16));
+                    UART_Trans((u8)((u_pres&0x0000FF00)>>8));
+                    UART_Trans((u8)u_pres&0x000000FF);
+                    UART_Trans(0x01);
+                    UART_Trans((u8)((u_depth&0xFF00)>>8));
+                    UART_Trans((u8)u_depth&0x00FF);
+                    UART_Trans(0xFF);
+                    UART_Trans(0xFF);
+                }
+            }
             ENABLE_SEND = 0;
         }
     }
